b0282b238fbbb103c60dbc81ca09491a5898de9c
Zhaozhen Zou <zzou@crcfe01.crc.nd.edu>
2019-10-16 12:16:30 -0400

    Add code using hmmsearch

--- bioinfo.sh
+++ bioinfo.sh
@@ -22,3 +22,13 @@ done
 ../hmmer-3.2.1/bin/hmmbuild hsp70gene.hmm hsp70gene.align
 ../hmmer-3.2.1/bin/hmmbuild mcrAgene.hmm mcrAgene.align
 
+# use hmmsearch to search genome for markers
+
+for file in ./proteomes/*.fasta
+do
+echo $file >> resultsmcrA.txt
+../hmmer-3.2.1/bin/hmmsearch --tblout mcrAgene.match mcrAgene.hmm $file | grep ">>" | wc -l >> resultsmcrA.txt
+echo $file >> resultshsp70.txt
+../hmmer-3.2.1/bin/hmmsearch --tblout hsp70gene.match hsp70gene.hmm $file | grep ">>" | wc -l >> resultshsp70.txt
+done
+
