dabc1ac89b9a3f268d579e72897cc1ad14f9887f
Zhaozhen Zou <zzou@crcfe01.crc.nd.edu>
2019-10-16 10:34:15 -0400

    Shell script can be run with wd in bioinfo folder

--- bioinfo.sh
+++ bioinfo.sh
@@ -1,16 +1,18 @@
+# Usage: bash bioinfo.sh
+
 # Create one reference file for each gene
 
-for file in mcrAgene*
+for file in ./ref_sequences/mcrAgene*
 do 
 cat $file >> mcrAgene.ref
 done
 
-for file in hsp70gene*
+for file in ./ref_sequences/hsp70gene*
 do 
 cat $file >> hsp70gene.ref
 done
 
 # Alignment using muscle
 
-../../muscle -in mcrAgene.ref -out mcrAgene.align
-../../muscle -in hsp70gene.ref -out hsp70gene.align
+../muscle -in mcrAgene.ref -out mcrAgene.align
+../muscle -in hsp70gene.ref -out hsp70gene.align
